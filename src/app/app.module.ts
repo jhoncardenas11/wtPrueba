d53e97e20fdda4ae9b5f838098c81e2e4721df89
unknown <jhoncardenas.11@gmail.com>
2020-06-10 15:39:53 -0500

    instal materialize, sweet alert and created components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { UsersComponent } from './components/users/users.component';
+import { AddUserComponent } from './components/add-user/add-user.component';
+import { UserViewComponent } from './components/user-view/user-view.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    UsersComponent,
+    AddUserComponent,
+    UserViewComponent
   ],
   imports: [
     BrowserModule,
